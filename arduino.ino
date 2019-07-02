6cea460957e13443174999b28cb9b51bd6e7667c
Anonymous1901 <vvprsd@gmail.com>
2019-07-02 07:45:44 +0530

    Defining all pins

---
+++ arduino.ino
@@ -0,0 +1,20 @@
+#define light_FR  14
+#define light_FL  15
+#define light_BL  16
+#define light_BR  17
+#define horn_Buzz 18
+
+#define ENA_m1 5
+#define ENB_m1 6
+#define ENA_m2 10
+#define ENB_m2 11
+
+#define IN_11  2
+#define IN_12  3
+#define IN_13  4
+#define IN_14  7
+
+#define IN_21  8
+#define IN_22  9
+#define IN_23  12
+#define IN_24  13 
